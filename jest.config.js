98eef57a585b7e10dbb3e7d598fd9240e99a4acc
Alex Lobera <alex.l@miro.com>
2023-04-12 17:54:12 +0200

    initial commit

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 const config = {
   testEnvironment: "jsdom",
+  transform: {
+    "\\.[jt]sx?$": "babel-jest",
+  },
 };
 
 module.exports = config;
--- src/app.test.js
+++ src/app.test.js
@@ -1,5 +1,10 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
 import { App } from "./App";
 
 test("App", () => {
-  expect(App()).toBeTruthy();
+  const element = document.createElement("div");
+  document.body.appendChild(element);
+  const root = ReactDOM.createRoot(element);
+  const comp = root.render(<App />);
 });
